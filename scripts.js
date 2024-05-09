70a64ad2c04e52ea7b99330a4cc75655e69ca0f4
Noxolo <wendyngcobo98@gmail.com>
2024-05-09 19:18:37 +0200

    Add error handlers for zero divider

--- scripts.js
+++ scripts.js
@@ -5,6 +5,17 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  const calculatedValue = Math.floor(dividend / divider); // use math.floor to remove decimals
-  result.innerText = calculatedValue;
+  const calculatedValue = Math.floor(dividend / divider);  // use math.floor to remove decimals
+  if (dividend == '' || divider == ''){
+    result.innerText = 'Division not performed. Both values are required in inputs. Try again';
+  } else{
+    result.innerText = calculatedValue;
+  }
+  if (divider == 0) {
+    result.innerText = 'Division not performed. Invalid number provided. Try again';
+    throw new Error('Division not performed. Invalid number provided. Try again');
+  }
+    
+    
+
 });
