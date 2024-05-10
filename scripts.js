c3666afb9c9ae5f5cc80c40d3f24ae652a98f03d
Noxolo <wendyngcobo98@gmail.com>
2024-05-10 10:43:49 +0200

    add classlist for error message

--- scripts.js
+++ scripts.js
@@ -11,17 +11,19 @@ form.addEventListener("submit", (event) => {
     if (dividend == '' || divider == ''){
 
       result.innerText = 'Division not performed. Both values are required in inputs. Try again'; //replace with this text if "true"
+      result.classList.add('error-message')
     } else{
-      result.innerText = calculatedValue; // else this if "false"
+      result.innerText = calculatedValue; // else this if "false"   
     }
-  
+    
     if (divider == 0) {
       result.innerText = 'Division not performed. Invalid number provided. Try again';
       throw new Error('Division not performed. Invalid number provided. Try again');
       
     }
     if(isNaN(dividend) || isNaN(divider)){ // Check if values are numbers or not 
-       document.body.innerText= "Something critical went wrong. Please reload the page"; // crash the site
+       document.body.innerHTML= `Something critical went wrong. Please reload the page`; // crash the site
+
        throw new Error('Something critical went wrong. Please reload the page'); // throw error
     }
     
